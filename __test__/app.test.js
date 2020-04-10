edb75a06216a000bde3448dd38afa25b44faf686
Jacob Harrington <jharrington4@gmail.com>
2020-04-09 20:32:27 -0700

    getByMonth aggration route working and tests passing

---
+++ __test__/app.test.js
@@ -0,0 +1,24 @@
+require('dotenv').config();	
+
+const request = require('supertest');	
+const app = require('../lib/app');	
+const connect = require('../lib/utils/connect');	
+const mongoose = require('mongoose');	
+
+describe('Fish routes', () => {	
+  beforeAll(() => {	
+    connect();	
+  });	
+
+  afterAll(() => {	
+    return mongoose.connection.close();	
+  });	
+ 
+  it('can get fish by month', () => {
+    return request(app)
+      .get('/api/v1/fish/?start=4&end=4')
+      .then(res => {
+        expect(res.body.length).toEqual(34);
+      });
+  });
+});
